bfbc883d59a9b630a91ab2776b215b6344fe374d
Ilona Pokrywka <ilo@MacBook-Air-Ilona.local>
2023-07-12 23:24:20 +0200

    Edycja kodu w Projekt 5.py.

--- Projekt 5.py
+++ Projekt 5.py
@@ -1,39 +1,45 @@
-# Napisz dla Amazona program, który automatycznie wyciąga wybrane informacje z wybranych stron internetowych, na przykład nazwy produktów ze stron Leroy Merlin. Użytkownik podaje jedynie adres strony oraz ścieżkę xpath, którą może łatwo skopiować w przeglądarce.
 
-# 1. Użyj biblioteki click, aby łatwiej było Ci odczytać url oraz xpath.
 
-# 2. Podziel kod na funkcje tak, aby można było go łatwo testować.
-
-# 3. Napisz kilka testów. Zacznij od tzw. happy path, tzn. najprostszego przypadku, a następnie przetestuj przypadki brzegowe.
-
-# 4. Wyciągając z HTML tekst, usuń białe znaki z początku i końca (poszukaj odpowiedniej metody na stringach) oraz zamień znaki końca linii na spacje.
-
-# 5. Wyświetl każdy znaleziony element HTML w osobnej linii.
-
-# 6. Użyj docstringów, aby udokumentować Twój kod. Jakie informacje Twoim zdaniem warto tam zawrzeć?
-
-# Hint: W razie problemów z cudzysłowami w XPATH pod Windows, możesz zamienić je na apostrofy, np.:
-#     "//div[@id='wartosc']" 
-# zamiast 
-#     '//div[@id="wartosc"]'
-
-# python3 '/Users/ilo/Desktop/PYTHON/Projekt 5.py' https://www.leroymerlin.pl '//*[@id="product-listing"]/div/a/h3/text()'
+# python3 '/Users/ilo/Desktop/PYTHON/Praktyczny_python_M05/Projekt 5.py' 'https://www.leroymerlin.pl/materialy-budowlane/materialy-budowlane-stan-surowy/piasek-keramzyt,a347.html' '//*[@id="product-listing"]//div[contains(@class, "catalog-card-container")]'
+# python3 '/Users/ilo/Desktop/PYTHON/Praktyczny_python_M05/Projekt 5.py' 'https://home.biedronka.pl/kuchnia/?utm_source=biedronkapl&utm_medium=www&utm_campaign=biedronka_home_menu&_ga=2.34961573.501035135.1689147025-1634869815.1689147023' '//*[@id="product-listing"]/div[contains(@class, "ProductList")]'
+#  python3 '/Users/ilo/Desktop/PYTHON/Praktyczny_python_M05/Projekt 5.py' 'https://homla.com.pl/salon.html' '//*[@id="maincontent"]/div/strong/a/text()'
+# python3 '/Users/ilo/Desktop/PYTHON/Praktyczny_python_M05/Projekt 5.py' 'https://www.leroymerlin.pl/okna-i-drzwi/parapety,a228.html' '//div[contains(@class, "ProductListProductBlock_wrapper__uqfrW")]'
 
 import click
 from lxml import html
 import requests
 
-def extract_text(content, xpath):
+def extract_text(content: str, xpath: str) -> list[str]:
+    """
+    Extracts text from the webpage content using the XPath expression.
+
+    Args:
+        content (str): The webpage content.
+        xpath (str): The XPath expression to locate elements containing the text.
+
+    Returns:
+        list[str]: The list of extracted texts.
+    """
     dom = html.fromstring(content)
     elements = dom.xpath(xpath)
+    # print(f'ELEMENTS: {elements}')
     cleaned_elements = [element.text_content().strip().replace('\n', '') for element in elements]
-    # print(cleaned_elements)
     return cleaned_elements
 
 @click.command()
 @click.argument('url')
 @click.argument('xpath')
-def main(url, xpath):
+def main(url: str, xpath: str) -> None:
+    """
+    Main function of the program.
+
+    Args:
+        url (str): The URL of the webpage.
+        xpath (str): The XPath expression to locate elements containing the text.
+
+    Returns:
+        None
+    """
     page = requests.get(url)
     content = page.text
     elements = extract_text(content, xpath)
